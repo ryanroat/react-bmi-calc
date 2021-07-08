793d9bf0fef4003728fa99434aae096f4ce007c1
Ryan Roat <ryanroat@gmail.com>
2021-07-08 14:52:12 -0400

    intial app layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Card, CardContent, Container, InputAdornment, TextField } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="container" maxWidth="md">
+        <header>
+          <h1>Body Mass Index calculator</h1>
+        </header>
+        <main>
+          <div className="inputs">
+            <TextField className="height"
+              type = "number"
+              variant = "outlined"
+              label = 'your height in inches'
+              endAdornment = {<InputAdornment position="end">In</InputAdornment>} />
+            <TextField className="weight"
+              type = "number"
+              variant = "outlined"
+              label = 'your weight in pounds'
+              endAdornment = {<InputAdornment position="end">Lbs</InputAdornment>} />
+          </div>
+          <div className="output">
+            <Card>
+              <CardContent>
+                Your BMI is 
+              </CardContent>
+            </Card>
+          </div>
+        </main>
+
+
+      </Container>
     </div>
   );
 }
