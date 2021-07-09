f7f030fd836533c52c5ca7ca433f6ddbe7e4f6a6
Ryan Roat <ryanroat@gmail.com>
2021-07-09 16:23:08 -0400

    logic to calculate target weight based on user desired BMI input added Button to switch calculation formula and input/output fields

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 
-import { Card, CardContent, Container, TextField } from '@material-ui/core'
+import { Button, Card, CardContent, Container, TextField } from '@material-ui/core'
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -9,13 +9,22 @@ import { Footer } from "./components/Footer";
 function App() {
   const [height, setHeight] = useState(70)
   const [weight, setWeight] = useState(170)
+  const [targetBMI, setTargetBMI] = useState(20)
+  const [calcToggled, setCalcToggled] = useState(false) // boolean to track which type of calculation to perform
 
   return (
     <div className="App">
       <Container className="container" maxWidth="xs">
         <Header />
         <main>
-          <div className="inputs">
+          <Button 
+            variant="contained"
+            color="default"
+            onClick={() => {setCalcToggled(!calcToggled)}}
+          >Toggle Calculation
+          </Button>
+          {calcToggled ? <h2>Calculate Target Weight</h2> : <h2>Calculate BMI</h2>}
+          <section className="inputs">
             <TextField className="height"
               type = "number"
               variant = "outlined"
@@ -23,21 +32,37 @@ function App() {
               value = {height}
               onChange={(e) => setHeight(e.target.value)}
             />
-            <TextField className="weight"
+            {calcToggled ? (
+              <TextField className="targetBMI"
               type = "number"
               variant = "outlined"
-              label = 'your weight in pounds'
-              value = {weight}
-              onChange={(e) => setWeight(e.target.value)}
-            />
-          </div>
-          <div className="output">
+              label = 'your target BMI'
+              value = {targetBMI}
+              onChange={(e) => setTargetBMI(e.target.value)}
+              />
+            ) : (
+              <TextField className="weight"
+                type = "number"
+                variant = "outlined"
+                label = 'your weight in pounds'
+                value = {weight}
+                onChange={(e) => setWeight(e.target.value)}
+              />
+            )}
+          </section>
+          <section className="output">
             <Card>
+              {calcToggled ? (
+                <CardContent>
+                  Your target weight is { (targetBMI * height**2 / 703).toFixed(1) }
+                </CardContent>
+              ) : (
               <CardContent>
                 Your BMI is { (weight / (height**2) * 703).toFixed(2) }
               </CardContent>
+              )}
             </Card>
-          </div>
+          </section>
         </main>
         <Footer />
       </Container>
